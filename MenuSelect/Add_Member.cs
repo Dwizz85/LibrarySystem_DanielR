5322ed3c932540506520466ac9f4e390001c707d
Dwizz85 <daniel@contrive.se>
2024-12-05 15:04:20 +0100

    Fifth commit

---
+++ MenuSelect/Add_Member.cs
@@ -0,0 +1,4 @@
+using System;
+using LibrarySystem_DanielR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
--- obj/Debug/net8.0/LibrarySystem_DanielR.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibrarySystem_DanielR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystem_DanielR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d419a0a8594b211fc600b18da6db60bbdf913be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb69ec9d9eb126ec9898e28accb0cb7657c28dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystem_DanielR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySystem_DanielR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
